1ea676b2ada6be6192e65e2b6057193a82c33481
Mokomcalixte <calixtemokom@gmail.com>
2024-05-15 22:53:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+import Routes from './config/routes';
+function App() {
+
+
+  return (
+    
+    <div>
+<Routes/>
+    </div>
+    
+  
+  );
+}
+
+export default App;
